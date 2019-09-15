b83d2ddfa18d0b0abdb84b4be84bc965bd1ab16a
thebosak <33043855+thebosak@users.noreply.github.com>
2019-09-14 20:38:38 -0500

    initial commit of main function

---
+++ main/Main.py
@@ -0,0 +1,13 @@
+'''
+Created on September 14, 2019
+
+@author: Brad Bosak
+'''
+
+#Create main function
+def main():
+    userInput = input('Please provide the phrase to print in the console: ')
+    print('The phrase is "' + userInput + '"')
+    
+#Call main function
+main()
